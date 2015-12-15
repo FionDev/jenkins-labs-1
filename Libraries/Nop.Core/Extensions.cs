55fce5b3c7ffef5ef6b5cd1faa0661b56b5691e6
FionDev <fiondev7@gmail.com>
2015-12-15 15:30:02 +0800

    test

--- Libraries/Nop.Core/Extensions.cs
+++ Libraries/Nop.Core/Extensions.cs
@@ -7,6 +7,7 @@ namespace Nop.Core
         public static bool IsNullOrDefault<T>(this T? value) where T : struct
         {
             return default(T).Equals(value.GetValueOrDefault());
+
         }
     }
 }
